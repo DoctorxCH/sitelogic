13a1a5bfcf52e185b5b4314bfdd8b6bcd6cb9ef5
ftp.admin@365jobs.sk <ftp.admin@365jobs.sk@ssh.hostcreators.sk>
2026-06-20 00:19:58 +0200

    storage/framework/views aus Git entfernt und .gitignore angepasst

--- storage/framework/views/0f0402824495940c895721fc51fe1f21.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['fi-dropdown-list p-1'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /var/www18/p50824/sitelogic.sk/web/vendor/filament/support/resources/views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
--- storage/framework/views/a853613fd5c1758b4067ecec56f99ee9.php
+++
@@ -1,8 +0,0 @@
-<h4
-    <?php echo e($attributes->class(['fi-ta-empty-state-heading text-base font-semibold leading-6 text-gray-950 dark:text-white'])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</h4>
-<?php /**PATH /var/www18/p50824/sitelogic.sk/web/vendor/filament/tables/resources/views/components/empty-state/heading.blade.php ENDPATH**/ ?>
